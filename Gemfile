ac9f01b14fec8aba2cfc27b3103823ac8ca5e382
ojirei <Rei@S-ReiOji-no-MacBook-Pro.local>
2016-12-18 22:55:38 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'rake'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'nokogiri'
+
+gem 'bcrypt'
+
+gem 'cleanup'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+gem 'pg'
+#gem 'rails_12factor'
+end
---
+++ models.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+Bundler.require
+
+if development?
+  ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+end
+
+class User < ActiveRecord::Base
+	has_secure_password
+end
+
+#unless ENV['RACK_ENV'] == 'production'
+#    ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+#end
+
